285ecc4b2968291fea9763f7944b8eae6739b778
rob-reynolds <rob@alphora.com>
2025-04-13 20:19:01 -0400

    WIP ast

--- src/ast/utils.ts
+++ src/ast/utils.ts
@@ -1,4 +1,4 @@
-import { ASTNode } from './types';
+import { ASTNode, BlockBody } from './types';
 
 interface ASTComparison {
   lineCountsMatch: boolean;
@@ -55,6 +55,14 @@ export function printAST(node: ASTNode, indent = 0): string {
   if ('action' in node && node.action) {
     const action = node.action as ASTNode;
     output += printAST(action, indent + 1);
+  }
+
+  // Handle BlockBody statements
+  if (node.type === 'BlockBody' && 'statements' in node) {
+    const blockBody = node as BlockBody;
+    blockBody.statements.forEach((statement: ASTNode) => {
+      output += printAST(statement, indent + 1);
+    });
   }
 
   return output;
