14005305d04fae49ccea75dc7a34f9acab3f1a7f
rob-reynolds <rob@alphora.com>
2025-04-15 15:08:30 -0400

    More validator tests passing

--- src/validator/tests/unusedConcepts.test.ts
+++ src/validator/tests/unusedConcepts.test.ts
@@ -13,7 +13,15 @@ describe('UnusedDeclarationsValidator - Concepts', () => {
   let validator: UnusedDeclarationsValidator;
 
   beforeEach(() => {
-    validator = new UnusedDeclarationsValidator();
+    const ast: File = {
+      type: FileType.type,
+      statements: [],
+      location: {
+        start: { line: 1, column: 1 },
+        end: { line: 1, column: 1 },
+      },
+    };
+    validator = new UnusedDeclarationsValidator(ast);
   });
 
   it('should detect unused concepts', () => {
