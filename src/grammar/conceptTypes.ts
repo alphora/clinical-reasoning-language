053197f709784c7574efd3ce17fb42fc9822ca93
rob-reynolds <rob@alphora.com>
2025-04-24 02:13:59 -0400

    Move auto generation files to a more standard location

--- src/grammar/conceptTypes.ts
+++ src/grammar/conceptTypes.ts
@@ -1,3 +1,3 @@
-import conceptTypesJson from './conceptTypes.json';
+import conceptTypesJson from './generated/types/conceptTypes.json';
 export const conceptTypes = conceptTypesJson as string[];
 export type ConceptType = typeof conceptTypes[number]; 
