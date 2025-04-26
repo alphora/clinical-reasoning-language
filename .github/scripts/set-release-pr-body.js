20b9bcc4d23ccf28b6092786b094f97eef804a4c
rob-reynolds <rob@alphora.com>
2025-04-26 17:08:05 -0400

    Fix github workflow

--- .github/scripts/set-release-pr-body.js
+++ .github/scripts/set-release-pr-body.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const body = fs.readFileSync(
-  path.join(__dirname, '../PULL_REQUEST_TEMPLATE/release.md'),
-  'utf8'
-);
+module.exports = async ({ github, context }) => {
+  const body = fs.readFileSync(
+    path.join(__dirname, '../PULL_REQUEST_TEMPLATE/release.md'),
+    'utf8'
+  );
 
-github.rest.pulls.update({
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  pull_number: context.issue.number,
-  body,
-});
+  await github.rest.pulls.update({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.issue.number,
+    body,
+  });
+};
