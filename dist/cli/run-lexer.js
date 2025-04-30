71d09e13d497d3eb0bf642b89d8609bd413f01e9
rob-reynolds <rob@alphora.com>
2025-04-30 17:23:37 -0400

    Run build for release

--- dist/cli/run-lexer.js
+++ dist/cli/run-lexer.js
@@ -4,7 +4,7 @@ const fs_1 = require("fs");
 const path_1 = require("path");
 const CRLLexer_1 = require("../grammar/generated/antlr/CRLLexer");
 const createLexer_1 = require("../lexer/createLexer");
-const examplePath = (0, path_1.join)(__dirname, "../examples/crl/who/smart-example-immz/IMMZ_All_Decisions.cpg");
+const examplePath = (0, path_1.join)(__dirname, "../examples/crl/who/smart-example-immz/IMMZ_All_Decisions.crl");
 const input = (0, fs_1.readFileSync)(examplePath, "utf8");
 const { lexer } = (0, createLexer_1.createLexer)(input);
 const tokens = [];
@@ -26,7 +26,7 @@ if (!prettyOutput) {
     console.log(JSON.stringify(tokens, null, 2));
 }
 else {
-    console.log("\nTokenizing grammar-example.cpg:\n");
+    console.log("\nTokenizing grammar-example.crl:\n");
     console.log("Line | Column | Type | Text");
     console.log("-----|--------|------|------");
     let lastLine = 0;
