9e84ff2df5e6673866053571a111a640f42a778b
rob-reynolds <rob@alphora.com>
2025-05-12 19:19:54 -0400

    Comments tests passing

--- src/lexer/tests/comments.test.ts
+++ src/lexer/tests/comments.test.ts
@@ -1,16 +1,15 @@
 import { CRLLexer } from "../../grammar/generated/antlr/CRLLexer";
 
-import { getTokensFromString } from "./helpers";
-import { verifyTokenSequence } from "./index.test";
+import { getTokensFromString, verifyTokenSequence } from "./helpers";
 
 describe("Comments", () => {
   describe("Single-line Comments", () => {
     it("should ignore single-line comments in decision blocks", () => {
-      const input = `decision "Test Decision"
+      const input = `decision "Test Decision":
     // This is a comment about the condition
     when "Condition" then
         // This is a comment about the action
-        do "Action"
+        recommend activity "Action".
 `;
 
       const tokens = getTokensFromString(input);
@@ -18,50 +17,56 @@ describe("Comments", () => {
       verifyTokenSequence(tokens, [
         CRLLexer.DECISION,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.COLON,
         CRLLexer.WHEN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.THEN,
-        CRLLexer.DO,
+        CRLLexer.RECOMMEND_ACTIVITY,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.DOT,
       ]);
     });
 
     it("should handle single-line comments at the start of crl", () => {
       const input = `// This is a comment
-decision "Test"
+decision "Test":
     when "Condition" then
-        do "Action"
+        recommend activity "Action".
 `;
       const tokens = getTokensFromString(input);
 
       verifyTokenSequence(tokens, [
         CRLLexer.DECISION,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.COLON,
         CRLLexer.WHEN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.THEN,
-        CRLLexer.DO,
+        CRLLexer.RECOMMEND_ACTIVITY,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.DOT,
       ]);
     });
   });
 
   describe("Block Comments", () => {
     it("should ignore block comments between tokens", () => {
-      const input = `decision /* block comment */ "Test" // line comment
+      const input = `decision /* block comment */ "Test": // line comment
     when "Condition" /* another comment */ then
-        do "Action"
+        recommend activity "Action".
 `;
       const tokens = getTokensFromString(input);
 
       verifyTokenSequence(tokens, [
         CRLLexer.DECISION,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.COLON,
         CRLLexer.WHEN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.THEN,
-        CRLLexer.DO,
+        CRLLexer.RECOMMEND_ACTIVITY,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.DOT,
       ]);
     });
 
@@ -79,13 +84,13 @@ decision "Test"
     });
 
     it("should ignore block comments in activity statements", () => {
-      const input = `activity /* name */ "Vaccinate" /* action */ perform /* type */ CPGImmunizationRequest /* end */ .`;
+      const input = `activity /* name */ "Vaccinate" /* action */ request /* type */ CPGImmunizationRequest /* end */ .`;
       const tokens = getTokensFromString(input);
 
       verifyTokenSequence(tokens, [
         CRLLexer.ACTIVITY,
         CRLLexer.QUOTED_STRING,
-        CRLLexer.PERFORM,
+        CRLLexer.REQUEST,
         CRLLexer.ACTIVITY_TYPE,
         CRLLexer.DOT,
       ]);
@@ -93,42 +98,42 @@ decision "Test"
 
     it("should ignore block comments in concept statements", () => {
       const input = `concept /* name */ "BMI" /* start */ :
-    /* type */ type is /* value */ Observation /* end */ .
-    /* valuetype */ valuetype /* value */ is Quantity /* end */ .
-    /* inference */ inferred from /* expr */ ("BMI Range" /* or */ or /* value */ "BMI Value") /* end */ .
-done`;
+    /* type */ - type is /* value */ Observation /* end */ .
+    /* valuetype */ - valuetype is Quantity /* end */ .
+    /* inference */ - inferred from /* expr */ ("BMI Range" /* or */ or /* value */ "BMI Value") /* end */ .`;
       const tokens = getTokensFromString(input);
 
       verifyTokenSequence(tokens, [
         CRLLexer.CONCEPT,
         CRLLexer.QUOTED_STRING,
         CRLLexer.COLON,
-        CRLLexer.TYPE,
-        CRLLexer.IS,
+        CRLLexer.DASH,
+        CRLLexer.TYPE_IS,
         CRLLexer.CONCEPT_TYPE,
         CRLLexer.DOT,
-        CRLLexer.VALUETYPE,
-        CRLLexer.IS,
+        CRLLexer.DASH,
+        CRLLexer.VALUETYPE_IS,
         CRLLexer.CONCEPT_VALUE_TYPE,
         CRLLexer.DOT,
-        CRLLexer.INFERRED,
-        CRLLexer.FROM,
+        CRLLexer.DASH,
+        CRLLexer.INFERRED_FROM,
         CRLLexer.LPAREN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.OR,
         CRLLexer.QUOTED_STRING,
         CRLLexer.RPAREN,
         CRLLexer.DOT,
-        CRLLexer.DONE,
       ]);
     });
   });
 
   describe("Comments in Expressions", () => {
     it("should handle comments between tokens in expressions", () => {
-      const input = `decision "Test"
-    when "Condition" then
-        do "Action" /* comment */ and /* another comment */ "Action 2"
+      const input = `decision "Test":
+    - when "Condition" then:
+        - recommend activity "Action" /* comment */.
+        - recommend activity "Action 2" /* another comment */.
+   - end when
 `;
 
       const tokens = getTokensFromString(input);
@@ -136,22 +141,31 @@ done`;
       verifyTokenSequence(tokens, [
         CRLLexer.DECISION,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.COLON,
+        CRLLexer.DASH,
         CRLLexer.WHEN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.THEN,
-        CRLLexer.DO,
+        CRLLexer.COLON,
+        CRLLexer.DASH,
+        CRLLexer.RECOMMEND_ACTIVITY,
         CRLLexer.QUOTED_STRING,
-        CRLLexer.AND,
+        CRLLexer.DOT,
+        CRLLexer.DASH,
+        CRLLexer.RECOMMEND_ACTIVITY,
         CRLLexer.QUOTED_STRING,
+        CRLLexer.DOT,
+        CRLLexer.DASH,
+        CRLLexer.END_WHEN,
       ]);
     });
 
     it("should handle comments in complex expressions", () => {
-      const input = `when /* start */ ("Condition 1" /* and */ and /* next */ "Condition 2" /* or */ or /* last */ "Condition 3") /* end */ then`;
+      const input = `inferred from /* start */ ("Condition 1" /* and */ and /* next */ "Condition 2" /* or */ or /* last */ "Condition 3") /* end */.`;
       const tokens = getTokensFromString(input);
 
       verifyTokenSequence(tokens, [
-        CRLLexer.WHEN,
+        CRLLexer.INFERRED_FROM,
         CRLLexer.LPAREN,
         CRLLexer.QUOTED_STRING,
         CRLLexer.AND,
@@ -159,7 +173,7 @@ done`;
         CRLLexer.OR,
         CRLLexer.QUOTED_STRING,
         CRLLexer.RPAREN,
-        CRLLexer.THEN,
+        CRLLexer.DOT,
       ]);
     });
   });
